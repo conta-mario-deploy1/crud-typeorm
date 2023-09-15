79d9f87f1ab01cc0e8e07467573fc298a977f7ec
conta-mario-deploy1 <conta.mario.deploy1@gmail.com>
2023-09-15 02:38:25 -0300

    project finalized

--- dist/config/data-source.js
+++ dist/config/data-source.js
@@ -24,7 +24,7 @@ exports.AppDataSource = new typeorm_1.DataSource({
         }
     },
     logging: true,
-    entities: ["./dist/entities/**/*.js"],
+    entities: ["dist/entities/Category.js", "dist/entities/Video.js"],
     migrations: ["src/database/migrations/**/*.ts"],
     subscribers: []
 });
