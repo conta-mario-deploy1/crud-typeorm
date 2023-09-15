b01b301f79a729f0b698e5544a2bd5cf6dce2169
conta-mario-deploy1 <conta.mario.deploy1@gmail.com>
2023-09-15 03:23:11 -0300

    project finalized

--- dist/repositories/category.repository.js
+++ dist/repositories/category.repository.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.categoryRepository = void 0;
 const data_source_1 = require("../config/data-source");
 const Category_entity_1 = require("../entities/Category.entity");
-exports.categoryRepository = data_source_1.AppDataSource.getRepository(Category_entity_1.Category);
+exports.categoryRepository = data_source_1.AppDataSource.getRepository(Category_entity_1.Category.entity);
